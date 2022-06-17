7cf8ae72cb869d5767adc3304c35b1ad169f8c73
tasnimultas67 <tasnimultas67@gmail.com>
2022-06-17 15:49:22 +0600

    3rd

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -7,11 +7,18 @@ const Cart = (props) => {
     for(const product of cart){
         total = total + product.price
     }
+    const shipping = total > 0 ? 15 : 0;
+    const tax = (total + shipping) *0.10;
+    const grandTotal = total + shipping + tax;
+
     return (
         <div>                
             <h2>Order Summary</h2>
             <p>Items Ordered: {props.cart.length} </p>
-            <h4>Total : {total}</h4>
+            <h4>Total : ${total}</h4>
+            <h4>Shipping : ${shipping}</h4>
+            <h4>Tax : ${tax}</h4>
+            <h3>Grand Total : ${grandTotal}</h3>
         </div>
     );
 };
--- src/Components/Pages/Shop/Shop.js
+++ src/Components/Pages/Shop/Shop.js
@@ -1,20 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../../utilities/fakedb';
 import Cart from '../../Cart/Cart';
 import Product from '../../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
     useEffect(()=>{
         fetch("products.JSON")
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([])
+    useEffect(()=>{
+        const savedCart = getStoredCart()
+        for(let key in savedCart){
+            const addedProduct = products.find(product => product.key === key)
+            console.log(key, addedProduct);
+
+        }
+    },[products])
+
 
     const handleAddToCart = product =>{
         const newCart = [...cart, product];
         setCart(newCart)
+        //save to local storage(for now)
+        addToDb(product.key)
     }
 
     return (
